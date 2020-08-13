f9ec60d43e49e0f1e1634a20c0b095ee939d5186
hushun1214 <2375353463@qq.com>
2020-08-13 11:25:22 +0800

    为导航网站添加背景图

--- nav-1/src/main.js
+++ nav-1/src/main.js
@@ -72,6 +72,7 @@ $(document).on("keypress", (e) => {
   for (let i = 0; i < hashMap.length; i++) {
     if (hashMap[i].logo.toLowerCase() === key) {
       window.open(hashMap[i].url);
+      break;
     }
   }
 });
